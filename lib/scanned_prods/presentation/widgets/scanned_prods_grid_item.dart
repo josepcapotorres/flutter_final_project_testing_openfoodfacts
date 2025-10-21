b22e19aa738646bfe8ece61e068a436ff4aba564
Josep <josepcapotorres@gmail.com>
2025-10-21 11:26:26 +0200

    style(scanned_prods): Add navigation to the detail page and add gradient colors to make the product name more visible

--- lib/scanned_prods/presentation/widgets/scanned_prods_grid_item.dart
+++ lib/scanned_prods/presentation/widgets/scanned_prods_grid_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../prods_scanning/presentation/pages/prods_scanning_prod_details_page.dart';
 import '../../domain/entities/scanned_prod.dart';
 
 class ScannedProdsGridItem extends StatelessWidget {
@@ -9,15 +10,44 @@ class ScannedProdsGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        FadeInImage(
-          placeholder: AssetImage("assets/images/loading.gif"),
-          image: NetworkImage(product.productImageUrl),
-          imageErrorBuilder: (_, __, ___) =>
-              Image.asset("assets/images/error.png"),
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(
+        context,
+        ProdsScanningProdDetailsPage.route,
+        arguments: product.barcode,
+      ),
+      child: Card(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            FadeInImage(
+              placeholder: AssetImage("assets/images/loading.gif"),
+              image: NetworkImage(product.productImageUrl),
+              imageErrorBuilder: (_, __, ___) =>
+                  Image.asset("assets/images/error.png"),
+            ),
+            Positioned.fill(
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.transparent, // Parte superior sin oscurecer
+                      Colors.black54, // Parte inferior más oscura
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(product.productName,
+                  style: TextStyle(color: Colors.white)),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
