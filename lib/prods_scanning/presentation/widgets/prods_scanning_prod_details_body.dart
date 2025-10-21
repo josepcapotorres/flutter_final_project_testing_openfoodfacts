269d18faf0e67f02e45a83cbca16e4d3df6032c4
Josep <josepcapotorres@gmail.com>
2025-10-21 11:31:28 +0200

    style(prods_scanning): Add more data to show in the details page

--- lib/prods_scanning/presentation/widgets/prods_scanning_prod_details_body.dart
+++ lib/prods_scanning/presentation/widgets/prods_scanning_prod_details_body.dart
@@ -9,22 +9,87 @@ class ProdsScanningProdDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final nutriscoreScore = loadedProduct.nutriscoreScore ?? "-";
+
     return SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 10,
-          children: [
-            if (loadedProduct.productImgUrl != null)
-              Image.network(
-                loadedProduct.productImgUrl!,
-                errorBuilder: (_, __, ___) =>
-                    Image.asset("assets/images/error.png"),
-              )
-            else
-              Image.asset("assets/images/error.png"),
-            Text(loadedProduct.productName ?? "-")
-          ],
-        ),
+      child: Column(
+        spacing: 10,
+        children: [
+          const SizedBox(
+            width: double.infinity,
+          ),
+          if (loadedProduct.productImgUrl != null)
+            Image.network(
+              loadedProduct.productImgUrl!,
+              errorBuilder: (_, __, ___) =>
+                  Image.asset("assets/images/error.png"),
+            )
+          else
+            Image.asset("assets/images/error.png"),
+          Text(
+            loadedProduct.productName ?? "-",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          ListTile(
+            title: Text("Puntuación de Nutriscore: asd asf asdf asdf asdf"),
+            subtitle: _NutriScoreValue(loadedProduct.nutriscoreScore),
+          ),
+          ListTile(
+            title: Text("Tipo de procesado: "),
+            subtitle: _ProcessedType(loadedProduct.processingFoodType),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class _NutriScoreValue extends StatelessWidget {
+  final String? score;
+
+  const _NutriScoreValue(this.score);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      score?.toUpperCase() ?? "-",
+      style: TextStyle(
+        fontSize: 22,
+        color: _getNutriScoreColors(),
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Color _getNutriScoreColors() {
+    return switch (score) {
+      'a' => Color(0xFF00833D),
+      'b' => Color(0xFF85BB2F),
+      'c' => Color(0xFFFECB02),
+      'd' => Color(0xFFEE8100),
+      'e' => Color(0xFFE63E11),
+      _ => Colors.black,
+    };
+  }
+}
+
+class _ProcessedType extends StatelessWidget {
+  final ProcessingFoodType processingFoodType;
+
+  const _ProcessedType(this.processingFoodType);
+
+  @override
+  Widget build(BuildContext context) {
+    final strProcessingType = switch (processingFoodType) {
+      ProcessingFoodType.withoutOrMinimumProcessed =>
+        "Alimentos no procesados o mínimamente procesados",
+      ProcessingFoodType.processedCulinaryIngs =>
+        "Ingredientes culinarios procesados",
+      ProcessingFoodType.processed => "Alimentos procesados",
+      ProcessingFoodType.ultraProcessed => "Alimentos ultraprocesados",
+      ProcessingFoodType.unknown => "Nova no calculado",
+    };
+
+    return Text(strProcessingType);
   }
 }
