3ec53ca8943e791acacdfcf552f2ddfb40444f3f
Josep <josepcapotorres@gmail.com>
2025-10-23 16:27:58 +0200

    fix(lib): Remove flavor param, given that we haven't used it yet

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,13 +1,6 @@
 class Environment {
-  static const String flavor = String.fromEnvironment(
-    'FLAVOR',
-    defaultValue: 'prod',
-  );
   static const String appName = String.fromEnvironment(
     'APP_NAME',
     defaultValue: 'OpenFoodFacts testing App',
   );
-
-  static bool get isTesters => flavor == 'testers';
-  static bool get isProd => flavor == 'prod';
 }
