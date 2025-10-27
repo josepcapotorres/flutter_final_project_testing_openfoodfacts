84b84397a2cbc97679bce0dd2d7f217794754d9a
Josep <josepcapotorres@gmail.com>
2025-10-27 11:53:06 +0100

    feat(crashlytics): Set up Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'environment.dart';
+import 'firebase_options.dart';
 import 'injection_container.dart' as di;
 import 'prods_scanning/presentation/pages/prods_scanning_prod_details_page.dart';
 import 'scanned_prods/presentation/pages/scanned_prods_page.dart';
@@ -9,6 +13,24 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await di.init();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  }
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(const MyApp());
 }
